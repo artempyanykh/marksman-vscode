1ca51754f03aa747e50ff14c3aa58631265a7c4b
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-03-11 09:36:52 +0000

    More reliable server restart; status bar item

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,39 @@
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
+import { LanguageClient, LanguageClientOptions, NotificationType, ServerOptions } from 'vscode-languageclient/node';
 
 import * as os from 'os';
 import * as path from 'path';
 
 let client: LanguageClient;
+let statusBarItem: vscode.StatusBarItem;
+
+type StatusParams = {
+	ok: boolean,
+	notes: number
+};
+
+const statusNotificationType = new NotificationType<StatusParams>("zeta-note/status");
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "zeta-note-vscode" is now active!');
 
 	let restartServerCmd = vscode.commands.registerCommand('zeta-note-vscode.restartServer', async () => {
-		await client.stop();
-		client.start();
-		vscode.window.showInformationMessage('Zeta Note server has been restarted');
+		stopClient(client);
+		resetStatus();
+
+		client = new LanguageClient("zeta-note", "Zeta Note", serverOptions, clientOptions, true);
+		configureClient(client);
+		context.subscriptions.push(client.start());
 	});
 	context.subscriptions.push(restartServerCmd);
+
+	let showOutputCmd = vscode.commands.registerCommand('zeta-note-vscode.showOutputChannel', () => {
+		if (client) {
+			let outputchannel = client.outputChannel;
+			outputchannel.show(true);
+		}
+	});
+	context.subscriptions.push(showOutputCmd);
 
 	let clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: "file", language: "markdown" }]
@@ -31,13 +50,56 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	};
 	client = new LanguageClient("zeta-note", "Zeta Note", serverOptions, clientOptions, true);
-	client.start();
+	configureClient(client);
+
+	createDefaultStatusBarItem();
+
+	context.subscriptions.push(
+		client.start(),
+		restartServerCmd,
+		showOutputCmd,
+	);
 }
 
-export function deactivate() {
-	if (!client) {
-		return undefined;
-	}
+function configureClient(client: LanguageClient) {
+	client.onReady().then(() => {
+		console.log("Client onReady");
 
-	return client.stop();
+		client.onNotification(statusNotificationType, (statusParams) => {
+			console.log("Got zeta-note/status notification");
+			updateStatus(statusParams);
+		});
+	});
+}
+
+function createDefaultStatusBarItem() {
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	statusBarItem.command = 'zeta-note-vscode.showOutputChannel';
+	resetStatus();
+	statusBarItem.show();
+}
+
+function updateStatus(statusParams: StatusParams) {
+	let status = statusParams.ok ? `✓ ZN (${statusParams.notes})` : '☠️ ZN';
+	statusBarItem.text = status;
+}
+
+function resetStatus() {
+	statusBarItem.text = "? ZN";
+}
+
+async function stopClient(client: LanguageClient) {
+	if (client) {
+		await client.stop();
+		client.outputChannel.dispose();
+		client.traceOutputChannel.dispose();
+	}
+}
+
+export async function deactivate() {
+	await stopClient(client);
+
+	if (statusBarItem) {
+		statusBarItem.hide();
+	}
 }
